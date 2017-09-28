LineageOS/android_device_htc_msm8974-common

msm8974-common: amplifier: Use headers from kernel
 * Apply the required changes to match the kernel drivers

Change-Id: I4f756b4a1dfee05f7beed6ff3e4ccfc987aa72dc

amplifier/rt5506.h
@@ -1,0 +1,24 @@
+/*
+ * Copyright (C) 2013-2015 The CyanogenMod Project
+ *               2017 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#pragma once
+
+#include <system/audio.h>
+
+#define RT5506_DEVICE "/dev/rt5501"
+
+int rt5506_set_mode(audio_mode_t mode);

AOKP/device_htc_msm8974-common

amplifier/rt5506.h
@@ -1,0 +1,25 @@
+/*
+ * Copyright (C) 2013-2015 The CyanogenMod Project
+ *               2017 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#pragma once
+
+#include <system/audio.h>
+
+#define RT55XX_DEVICE "/dev/rt5501"
+
+int rt5506_open(void);
+int rt5506_set_mode(audio_mode_t mode);